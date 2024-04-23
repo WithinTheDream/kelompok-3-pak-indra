44a1368146572020e36b3ec66aab4848fce7f404
WithinTheDream <148210628+WithinTheDream@users.noreply.github.com>
2024-04-23 12:42:27 +0700

    test1

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const products = [
     { name: 'Product 2', image: 'product2.jpg', description: 'Description of Product 2' },
     { name: 'Product 3', image: 'product3.jpg', description: 'Description of Product 3' },
     { name: 'Product 4', image: 'product4.jpg', description: 'Description of Product 4' },
+
+    { name: 'Product 5', image: 'product5.jpg', description: 'Description of Product 5' },
+    { name: 'Product 6', image: 'product6.jpg', description: 'Description of Product 6' },
+    { name: 'Product 7', image: 'product7.jpg', description: 'Description of Product 7' },
+    { name: 'Product 8', image: 'product8.jpg', description: 'Description of Product 8' },
+
+    { name: 'Product 1', image: 'product1.jpg', description: 'Description of Product 1' },
+    { name: 'Product 2', image: 'product2.jpg', description: 'Description of Product 2' },
+    { name: 'Product 3', image: 'product3.jpg', description: 'Description of Product 3' },
+    { name: 'Product 4', image: 'product4.jpg', description: 'Description of Product 4' },
     // Add more products here
 ];
 
